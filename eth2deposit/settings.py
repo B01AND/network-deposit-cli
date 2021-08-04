b0f9656eb1bf1903467e094078121c428e1560cb
mxmar <macht4tech@gmail.com>
2021-08-04 13:24:45 +0200

    Feature: l15-testnet deposit cli - implemented LUKSO l15-testnet network configuration;
    Signed-off-by: mxmar <macht4tech@gmail.com>

--- eth2deposit/settings.py
+++ eth2deposit/settings.py
@@ -12,6 +12,7 @@ class BaseChainSetting(NamedTuple):
 MAINNET = 'mainnet'
 PYRMONT = 'pyrmont'
 PRATER = 'prater'
+LUKSO = 'lukso-private-testnet'
 
 
 # Eth2 Mainnet setting
@@ -20,12 +21,15 @@ MainnetSetting = BaseChainSetting(ETH2_NETWORK_NAME=MAINNET, GENESIS_FORK_VERSIO
 PyrmontSetting = BaseChainSetting(ETH2_NETWORK_NAME=PYRMONT, GENESIS_FORK_VERSION=bytes.fromhex('00002009'))
 # Eth2 testnet (spec v1.0.1)
 PraterSetting = BaseChainSetting(ETH2_NETWORK_NAME=PRATER, GENESIS_FORK_VERSION=bytes.fromhex('00001020'))
+# LUKSO l15-testnet
+LuksoSetting = BaseChainSetting(ETH2_NETWORK_NAME=LUKSO, GENESIS_FORK_VERSION=bytes.fromhex('50403020'))
 
 
 ALL_CHAINS: Dict[str, BaseChainSetting] = {
     MAINNET: MainnetSetting,
     PYRMONT: PyrmontSetting,
     PRATER: PraterSetting,
+    LUKSO: LuksoSetting,
 }
 
 
