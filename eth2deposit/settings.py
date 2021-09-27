209c299afcc70ad84bbefd44878599f567b233e3
mxmar <macht4tech@gmail.com>
2021-09-27 15:39:34 +0200

    Added dev, staging and prod network settings;
    Signed-off-by: mxmar <macht4tech@gmail.com>

--- eth2deposit/settings.py
+++ eth2deposit/settings.py
@@ -1,7 +1,7 @@
 from typing import Dict, NamedTuple
 
 
-DEPOSIT_CLI_VERSION = '1.2.0-lukso'
+DEPOSIT_CLI_VERSION = '1.2.1-lukso'
 
 
 class BaseChainSetting(NamedTuple):
@@ -12,7 +12,9 @@ class BaseChainSetting(NamedTuple):
 MAINNET = 'mainnet'
 PYRMONT = 'pyrmont'
 PRATER = 'prater'
-LUKSO = 'lukso-private-testnet'
+L15_LUKSO_DEV = 'l15-testnet-dev'
+L15_LUKSO_STAGING = 'l15-testnet-staging'
+L15_LUKSO_PROD = 'l15-testnet-prod'
 
 
 # Eth2 Mainnet setting
@@ -21,15 +23,21 @@ MainnetSetting = BaseChainSetting(ETH2_NETWORK_NAME=MAINNET, GENESIS_FORK_VERSIO
 PyrmontSetting = BaseChainSetting(ETH2_NETWORK_NAME=PYRMONT, GENESIS_FORK_VERSION=bytes.fromhex('00002009'))
 # Eth2 testnet (spec v1.0.1)
 PraterSetting = BaseChainSetting(ETH2_NETWORK_NAME=PRATER, GENESIS_FORK_VERSION=bytes.fromhex('00001020'))
-# LUKSO l15 lukso-private-testnet
-LuksoSetting = BaseChainSetting(ETH2_NETWORK_NAME=LUKSO, GENESIS_FORK_VERSION=bytes.fromhex('83a55317'))
+# LUKSO l15 testnet dev
+LuksoDevSetting = BaseChainSetting(ETH2_NETWORK_NAME=L15_LUKSO_DEV, GENESIS_FORK_VERSION=bytes.fromhex('63a55317'))
+# LUKSO l15 testnet staging
+LuksoStagingSetting = BaseChainSetting(ETH2_NETWORK_NAME=L15_LUKSO_STAGING, GENESIS_FORK_VERSION=bytes.fromhex('73a55317'))
+# LUKSO l15 testnet prod
+LuksoProdSetting = BaseChainSetting(ETH2_NETWORK_NAME=L15_LUKSO_PROD, GENESIS_FORK_VERSION=bytes.fromhex('83a55317'))
 
 
 ALL_CHAINS: Dict[str, BaseChainSetting] = {
     MAINNET: MainnetSetting,
     PYRMONT: PyrmontSetting,
     PRATER: PraterSetting,
-    LUKSO: LuksoSetting,
+    L15_LUKSO_DEV: LuksoDevSetting,
+    L15_LUKSO_STAGING: LuksoStagingSetting,
+    L15_LUKSO_PROD: LuksoProdSetting,
 }
 
 
