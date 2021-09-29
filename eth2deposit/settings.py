05a8b08d5c04ed1d49275d37459d63001c208b8b
mxmar <macht4tech@gmail.com>
2021-09-29 12:29:13 +0200

    Modifications for L15 LUKSO dev, staging and prod network configs;
    Signed-off-by: mxmar <macht4tech@gmail.com>

--- eth2deposit/settings.py
+++ eth2deposit/settings.py
@@ -1,7 +1,7 @@
 from typing import Dict, NamedTuple
 
 
-DEPOSIT_CLI_VERSION = '1.2.1-lukso'
+DEPOSIT_CLI_VERSION = '1.2.5-LUKSO'
 
 
 class BaseChainSetting(NamedTuple):
@@ -12,9 +12,9 @@ class BaseChainSetting(NamedTuple):
 MAINNET = 'mainnet'
 PYRMONT = 'pyrmont'
 PRATER = 'prater'
-L15_LUKSO_DEV = 'l15-testnet-dev'
-L15_LUKSO_STAGING = 'l15-testnet-staging'
-L15_LUKSO_PROD = 'l15-testnet-prod'
+L15_DEV = 'l15-dev'
+L15_STAGING = 'l15-staging'
+L15_PROD = 'l15-prod'
 
 
 # Eth2 Mainnet setting
@@ -23,21 +23,21 @@ MainnetSetting = BaseChainSetting(ETH2_NETWORK_NAME=MAINNET, GENESIS_FORK_VERSIO
 PyrmontSetting = BaseChainSetting(ETH2_NETWORK_NAME=PYRMONT, GENESIS_FORK_VERSION=bytes.fromhex('00002009'))
 # Eth2 testnet (spec v1.0.1)
 PraterSetting = BaseChainSetting(ETH2_NETWORK_NAME=PRATER, GENESIS_FORK_VERSION=bytes.fromhex('00001020'))
-# LUKSO l15 testnet dev
-LuksoDevSetting = BaseChainSetting(ETH2_NETWORK_NAME=L15_LUKSO_DEV, GENESIS_FORK_VERSION=bytes.fromhex('63a55317'))
-# LUKSO l15 testnet staging
-LuksoStagingSetting = BaseChainSetting(ETH2_NETWORK_NAME=L15_LUKSO_STAGING, GENESIS_FORK_VERSION=bytes.fromhex('73a55317'))
-# LUKSO l15 testnet prod
-LuksoProdSetting = BaseChainSetting(ETH2_NETWORK_NAME=L15_LUKSO_PROD, GENESIS_FORK_VERSION=bytes.fromhex('83a55317'))
+# LUKSO l15 dev
+L15DevSetting = BaseChainSetting(ETH2_NETWORK_NAME=L15_DEV, GENESIS_FORK_VERSION=bytes.fromhex('63a55317'))
+# LUKSO l15 staging
+L15StagingSetting = BaseChainSetting(ETH2_NETWORK_NAME=L15_STAGING, GENESIS_FORK_VERSION=bytes.fromhex('73a55317'))
+# LUKSO l15 prod
+L15ProdSetting = BaseChainSetting(ETH2_NETWORK_NAME=L15_PROD, GENESIS_FORK_VERSION=bytes.fromhex('83a55317'))
 
 
 ALL_CHAINS: Dict[str, BaseChainSetting] = {
     MAINNET: MainnetSetting,
     PYRMONT: PyrmontSetting,
     PRATER: PraterSetting,
-    L15_LUKSO_DEV: LuksoDevSetting,
-    L15_LUKSO_STAGING: LuksoStagingSetting,
-    L15_LUKSO_PROD: LuksoProdSetting,
+    L15_DEV: L15DevSetting,
+    L15_STAGING: L15StagingSetting,
+    L15_PROD: L15ProdSetting,
 }
 
 
